97ae28db295926a3a216a03dc2c2316017afebae
Kirill <79154700+Ko-Ki-Ser@users.noreply.github.com>
2021-03-28 19:37:33 +0700

    Create errorproc.c

---
+++ errorproc.c
@@ -0,0 +1,62 @@
+#include "errorproc.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+
+int Socket (int domain, int type, int protocol){
+	int res = socket (domain, type, protocol);
+	if (res==-1){
+		perror("socket failed");
+		exit(EXIT_FAILURE);
+	}
+	return res;
+}
+
+void Bind (int sockfd, const struct sockaddr *addr, socklen_t addrlen){
+	int res = bind(sockfd, addr, addrlen);
+	if (res==-1){
+		perror("bind failed");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void Listen (int sockfd, int backlog){
+	int res = listen (sockfd, backlog);
+	if (res==-1){
+		perror("listen failed");
+		exit(EXIT_FAILURE);
+	}
+}
+
+int Accept (int sockfd, struct sockaddr *addr, socklen_t *addrlen){
+	int res = accept (sockfd, addr, addrlen);
+	if (res==-1){
+		perror("accept failed");
+		exit(EXIT_FAILURE);
+	}
+	return res;
+}
+
+void Connect(int sockfd, const struct sockaddr *addr, socklen_t addrlen){
+	int res = connect (sockfd, addr, addrlen);
+	if (res == -1){
+		perror("connection failed");
+		exit(EXIT_FAILURE);
+	}
+}
+
+void Inet_pton(int af, const char *src, void *dst){
+	int res = inet_pton(af, src, dst);
+	if (res == 0){
+		printf("inet_pton failed without ERNO\n");
+		exit(EXIT_FAILURE);
+	}
+	if (res == -1){
+		perror("inet_pton failed ERNO\n");
+		exit(EXIT_FAILURE);
+	}
+}
